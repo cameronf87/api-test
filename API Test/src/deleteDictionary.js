514da9b8c57045f03ba1d5dfd67680d7cecef464
Cameron <2974614+cameronf87@users.noreply.github.com>
2021-03-23 16:06:50 -0500

    created deleteDictionary.js example

--- API Test/src/deleteDictionary.js
+++ API Test/src/deleteDictionary.js
@@ -1,15 +1,17 @@
 //Save the private key to a parent folder and don't include in the git repository
-import token from '../../../private/keys/token.mjs';
+import token from '../../private/keys/token.mjs';
 //Import Axios node module
 import axios from 'axios';
 
 //Post to the API and log response
-axios.put('https://dictionary.iachieved.it/dictionary' ,{
-    //Data goes here
-    }, {
+axios.delete('https://dictionary.iachieved.it/dictionary/id/',
+{
     headers: {
         'Authorization' : token
-    }
+    },
+    data: {
+        id: '147775b7-f3d6-41c4-ae38-3fd899ebba34' 
+    },
 })
     //returns a promise, if successful log the data
     .then(response => console.log(response.data))
